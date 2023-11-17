4c43cb1611d80b66d5012e7cd3b81f6fb150c54c
smlk.dev <1147241-eskaapps@users.noreply.gitlab.com>
2023-11-17 02:25:30 +0100

    Increase video difficulty.
    Co-authored-by: smlk.dev <48448829+smlkdev@users.noreply.github.com>

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,4 +1,5 @@
-import {AbsoluteFill} from 'remotion';
+import {interpolate} from 'remotion';
+import {AbsoluteFill, Img, staticFile, useCurrentFrame} from 'remotion';
 import {Logo} from './Logo';
 import {Subtitle} from './Subtitle';
 import {Title} from './Title';
@@ -16,13 +17,24 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
 	titleColor: propTwo,
 	logoColor: propThree,
 }) => {
+	const frame = useCurrentFrame();
+	const scaleMandalas = interpolate(frame, [0, 300, 600, 900], [1, 2, 1, 2]);
+
 	return (
-		<AbsoluteFill className="bg-gray-100 items-center justify-center">
-			<div className="m-10" />
-			<Logo logoColor={propThree} />
-			<div className="m-3" />
-			<Title titleText={propOne} titleColor={propTwo} />
-			<Subtitle />
+		<AbsoluteFill className="items-center justify-center bg-gray-100">
+			<AbsoluteFill>
+				<Img
+					src={staticFile('mandalas.jpg')}
+					style={{scale: `${scaleMandalas}`}}
+				/>
+			</AbsoluteFill>
+			<AbsoluteFill className="items-center justify-center">
+				<div className="m-10" />
+				<Logo logoColor={propThree} />
+				<div className="m-3" />
+				<Title titleText={propOne} titleColor={propTwo} />
+				<Subtitle />
+			</AbsoluteFill>
 		</AbsoluteFill>
 	);
 };
